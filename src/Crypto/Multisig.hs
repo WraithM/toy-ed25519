27fe7aea9cdd2b2f7a57a711d18e2645cd223129
Matthew Wraith <matt@bitnomial.com>
2018-01-21 18:45:32 -0600

    Simple delinearized multisig
    https://eprint.iacr.org/2018/068

--- src/Crypto/Multisig.hs
+++ src/Crypto/Multisig.hs
@@ -2,10 +2,12 @@
 
 module Crypto.Multisig where
 
+import           Crypto.Hash     (Digest, SHA256, hash)
 import           Crypto.Random   (getRandomBytes)
-import           Data.ByteArray  (pack, unpack)
+import           Data.ByteArray  (convert, pack, unpack)
 import           Data.ByteString (ByteString)
-import           Data.Monoid     (Monoid, mconcat)
+import           Data.List       (sort)
+import           Data.Monoid     (Monoid, mconcat, (<>))
 
 import           Crypto.Ed25519
 
@@ -29,18 +31,26 @@ combineNonces = mconcat
 newtype PartialSignature = PartialSignature { unPartialSig :: Integer } deriving (Show, Eq)
 
 
+multisigPublicKey :: [PublicKey] -> PublicKey
+multisigPublicKey pubKeys = mconcat $ map delinearizePubkey pubKeys
+  where
+    delinearizePubkey pubKey = PublicKey $ delinearCoeff pubKeys pubKey `scalarMultiply` publicKeyPoint pubKey
+
+
 signMultisig :: PrivateKey  -- ^ Private key for partial signature
              -> Nonce       -- ^ Private nonce
              -> PublicNonce -- ^ Sum of all public nonces
-             -> PublicKey   -- ^ Sum of all public keys
+             -> [PublicKey] -- ^ All of the public keys
              -> Message     -- ^ Message to sign
              -> PartialSignature
-signMultisig (PrivateKey k) (Nonce r) (PublicNonce pR) pubKeys m = PartialSignature s
+signMultisig privKey@(PrivateKey k) (Nonce r) (PublicNonce pR) pubKeys m = PartialSignature s
   where
+    pubKey = publicKey privKey
+
     h = unpack $ sha512 k
     lsbHashInt = fromBytes . pack $ drop b h
 
-    s = (r + hram pR pubKeys m * lsbHashInt) `mod` l
+    s = (r + delinearCoeff pubKeys pubKey * hram pR (multisigPublicKey pubKeys) m * lsbHashInt) `mod` l
 
 
 combineMultisig :: PublicNonce -> [PartialSignature] -> Signature
@@ -48,4 +58,14 @@ combineMultisig (PublicNonce pR) = Signature pR . sum . map unPartialSig
 
 
 verifyMultisig :: [PublicKey] -> Message -> Signature -> Bool
-verifyMultisig = verify . mconcat
+verifyMultisig = verify . multisigPublicKey
+
+
+delinearCoeff :: [PublicKey] -> PublicKey -> Integer
+delinearCoeff pubKeys pubKey = h0 $ serializedPubKeys pubKeys <> encodePublicKey pubKey
+  where
+    serializedPubKeys = sha256Digest . mconcat . sort . map encodePublicKey
+    h0 = fromBytes . sha256Digest
+    sha256Digest = convert . sha256
+    sha256 :: ByteString -> Digest SHA256
+    sha256 = hash
